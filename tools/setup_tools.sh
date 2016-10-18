52dfc61399f83ee4017c338c8bca909a00853c1e
gradiuscypher <gradiuscypher@gmail.com>
2016-10-18 09:34:53 -0700

    removing old files no longer relevant to the project

--- tools/setup_tools.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.2.0/elasticsearch-2.2.0.tar.gz
-wget https://download.elastic.co/kibana/kibana/kibana-4.4.1-linux-x64.tar.gz
-tar xzf elasticsearch-2.2.0.tar.gz
-tar xzf kibana-4.4.1-linux-x64.tar.gz
-rm elasticsearch-2.2.0.tar.gz
-rm kibana-4.4.1-linux-x64.tar.gz
